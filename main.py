87386b6cc681f8ffe87d6afa8f14a4c950a2d00e
даниэль <dannilboyar@gmail.com>
2024-10-24 10:26:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def are_anagrams(str1, str2):
-  return sorted(str1.lower()) == sorted(str2.lower())
+str1 = input("Введите первую строку: ")
+str2 = input("Введите вторую строку: ")
 
-str1 = input("Введите первую строку:" )
-str2 = input("Введите вторую строкуЖ" )
-
-if are_anagrams(str1 , str2):
+if sorted(str1.lower()) == sorted(str2.lower()):
   print("Строки являются анаграммами.")
 else:
   print("Строки не являются анаграммами.")
